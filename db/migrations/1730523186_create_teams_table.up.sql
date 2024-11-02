6e6f19d5daf70eb7991f86ebf24a0b85d9d80a98
Garrett Bodley <garrett.bodley@gmail.com>
2024-11-02 17:23:46 -0400

    add more columns to teams table.

--- db/migrations/1730523186_create_teams_table.up.sql
+++ db/migrations/1730523186_create_teams_table.up.sql
@@ -1,5 +1,15 @@
-CREATE TABLE teams (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL UNIQUE,
-  teamid INTEGER UNIQUE
-);
+CREATE TABLE
+  IF NOT EXISTS teams (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    team_id INTEGER UNIQUE,
+    team_city STRING,
+    team_name STRING,
+    team_abbreviation STRING,
+    team_conference STRING,
+    team_division STRING,
+    team_code STRING,
+    team_slug STRING,
+    min_year INT,
+    max_year INT
+  );
